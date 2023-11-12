ac4914a8599f05c02382c87d8665315e48d4705d
Elvin Rey Saavedra <elvinreysa@gmail.com>
2023-11-12 18:03:33 +1000

    created an own gpt

---
+++ backend/README.md
@@ -0,0 +1,4 @@
+
+# MERN Stack AI Chatbot
+
+Trying out ChatGPT api to create a chatbot
---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Logo from "./shared/Logo";
+import { useAuth } from "../context/AuthContext";
+import NavigationLink from "./shared/NavigationLink";
+import { useLocation } from "react-router-dom";
+
+const Header = () => {
+  const auth = useAuth();
+  const location = useLocation();
+  console.log(location)
+  return (
+    <AppBar
+      sx={{ bgcolor: "transparent", position: "static", boxShadow: "none" }}
+    >
+      <Toolbar sx={{ display: "flex" }}>
+        <Logo />
+        <div>
+          {auth?.isLoggedIn ? (
+            <>
+              {location.pathname !== "/chat" && <NavigationLink
+                bg="#008170"
+                to="/chat"
+                text="Go To Chat"
+                textColor="white"
+              />}
+              <NavigationLink
+                bg="#51538f"
+                textColor="white"
+                to="/"
+                text="logout"
+                onClick={auth.logout}
+              />
+            </>
+          ) : (
+            <>
+              <NavigationLink
+                bg="#00fffc"
+                to="/login"
+                text="Login"
+                textColor="black"
+              />
+              <NavigationLink
+                bg="#51538f"
+                textColor="white"
+                to="/signup"
+                text="Signup"
+              />
+            </>
+          )}
+        </div>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Header;
